ae03cec45230942ef7e5e4f2bd0aaad616e88015
Julian Hindelang <hindelang_julian@hotmail.de>
2019-11-16 19:01:47 +0100

    removed useless helper functions

--- src/dbcppp/Helper.cpp
+++ src/dbcppp/Helper.cpp
@@ -8,11 +8,3 @@ bool SharedNodeCmp::operator()(const std::shared_ptr<Node>& lhs, const std::shar
 {
 	return lhs->name < rhs->name;
 }
-bool operator==(const std::shared_ptr<Node>& lhs, const std::string& rhs)
-{
-	return lhs->name == rhs;
-}
-bool operator==(const std::string& lhs, const std::shared_ptr<Node>& rhs)
-{
-	return lhs == rhs->name;
-}
