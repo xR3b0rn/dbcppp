6683ae333eec8c339671b89de7ebbc6daa23d362
xR3b0rn <6249046+xR3b0rn@users.noreply.github.com>
2019-11-19 17:47:57 +0100

    using boost::endian::endian_reverse instead self programmed byte swap function, since boost is using intrinsic functions for byte swaping if present

--- src/dbcppp/Signal.cpp
+++ src/dbcppp/Signal.cpp
@@ -1,15 +1,8 @@
 
 #include "Signal.h"
+#include <boost/endian/conversion.hpp>
 
 using namespace dbcppp;
-
-static uint64_t reverse_byte_order(uint64_t x)
-{
-    x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
-    x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
-    x = (x & 0x00FF00FF00FF00FF) << 8  | (x & 0xFF00FF00FF00FF00) >> 8;
-    return x;
-}
 
 uint64_t Signal::decode(uint64_t data) const
 {
@@ -17,7 +10,7 @@ uint64_t Signal::decode(uint64_t data) const
 	uint64_t mask = ~((uint64_t)-1 << bit_size);
 	if (byte_order == ByteOrder::BigEndian)
 	{
-		data = reverse_byte_order(data);
+		data = boost::endian::endian_reverse(data);
 		uint64_t sb = (8 * (7 - (start_bit / 8))) + (start_bit % 8) - (bit_size - 1);
 		result = (data >> sb) & mask;
 	}
@@ -36,7 +29,7 @@ void Signal::encode(uint64_t* data, uint64_t raw) const
 		uint64_t sb = (8 * (7 - (start_bit / 8))) + (start_bit % 8) - (bit_size - 1);
 		raw <<= sb;
 		*data &= ~(mask << sb);
-		*data |= reverse_byte_order(raw);
+		*data |= boost::endian::endian_reverse(raw);
 	}
 	else
 	{
