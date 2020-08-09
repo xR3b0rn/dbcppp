320fe2e2d2925c5682b18ff339b8193283a5d170
xR3b0rn <hindelang_julian@hotmail.de>
2020-08-09 18:31:54 +0200

    c

--- src/libdbcppp/SignalImpl.cpp
+++ src/libdbcppp/SignalImpl.cpp
@@ -312,10 +312,6 @@ std::unique_ptr<Signal> Signal::create(
         , std::move(value_descriptions)
         , std::move(comment)
         , extended_value_type);
-    if (result->getError() != SignalImpl::ErrorCode::NoError)
-    {
-        result = nullptr;
-    }
     return result;
 }
 SignalImpl::SignalImpl(
@@ -364,7 +360,7 @@ SignalImpl::SignalImpl(
     case ByteOrder::LittleEndian:
         if ((start_bit + bit_size) > message_size * 8)
         {
-            _error = ErrorCode::SignalExceedsMessageSize;
+            setError(ErrorCode::SignalExceedsMessageSize);
         }
         break;
     case ByteOrder::BigEndian:
@@ -372,7 +368,7 @@ SignalImpl::SignalImpl(
         int64_t fstart = int64_t(start_bit) - (start_bit % 8);
         if (fstart + ((fsize - 1) / 8) * 8 >= message_size * 8)
         {
-            _error = ErrorCode::SignalExceedsMessageSize;
+            setError(ErrorCode::SignalExceedsMessageSize);
         }
         break;
     }
@@ -381,23 +377,23 @@ SignalImpl::SignalImpl(
     case Signal::ExtendedValueType::Float:
         if (bit_size != 32)
         {
-            _error = ErrorCode::WrongBitSizeForExtendedDataType;
+            setError(ErrorCode::WrongBitSizeForExtendedDataType);
         }
         break;
     case Signal::ExtendedValueType::Double:
         if (bit_size != 64)
         {
-            _error = ErrorCode::WrongBitSizeForExtendedDataType;
+            setError(ErrorCode::WrongBitSizeForExtendedDataType);
         }
         break;
     }
     if (extended_value_type == ExtendedValueType::Float && !std::numeric_limits<float>::is_iec559)
     {
-        _error = ErrorCode::MaschinesFloatEncodingNotSupported;
+            setError(ErrorCode::MaschinesFloatEncodingNotSupported);
     }
     if (extended_value_type == ExtendedValueType::Double && !std::numeric_limits<double>::is_iec559)
     {
-        _error = ErrorCode::MaschinesDoubleEncodingNotSupported;
+            setError(ErrorCode::MaschinesDoubleEncodingNotSupported);
     }
 
     // save some additional values to speed up decoding
@@ -621,4 +617,8 @@ Signal::ExtendedValueType SignalImpl::getExtendedValueType() const
 bool SignalImpl::getError(ErrorCode code) const
 {
     return code == _error || (uint64_t(_error) & uint64_t(code));
+}
+void SignalImpl::setError(ErrorCode code)
+{
+    _error = ErrorCode(uint64_t(_error) | uint64_t(code));
 }
