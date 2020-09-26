d02cf23f2de13eec555bf4676245b574280a06e2
Byunghun Lee <byunghun.lee@magna.com>
2020-09-26 13:59:08 -0400

    test case reformatting

--- src/Test/DBCWithSingleLineCommentsParsing.cpp
+++ src/Test/DBCWithSingleLineCommentsParsing.cpp
@@ -1,4 +1,3 @@
-
 #include <array>
 #include <fstream>
 #include <iostream>
@@ -19,17 +18,17 @@ namespace utf = boost::unit_test;
 
 BOOST_AUTO_TEST_CASE(ParsingCppApiWithSingleLineComments)
 {
-	std::ifstream dbc_file(TEST_DBC_WITH_SINGLE_COMMENTS);
-	
-	BOOST_TEST_MESSAGE("Testing dbcppp C and C++ APIs for skipping a DBC with single line comments...");
+    std::ifstream dbc_file(TEST_DBC_WITH_SINGLE_COMMENTS);
 
-	const dbcppp_Network* impl = dbcppp_NetworkLoadDBCFromFile(TEST_DBC_WITH_SINGLE_COMMENTS);
-	BOOST_REQUIRE_MESSAGE(impl, "C API DBC parsing failed!");
+    BOOST_TEST_MESSAGE("Testing dbcppp C and C++ APIs for skipping single line comments...");
 
-	auto spec = dbcppp::Network::fromDBC(dbc_file);
-	BOOST_REQUIRE_MESSAGE(spec, "C++ API DBC parsing failed!");
-	
-	BOOST_REQUIRE_EQUAL(spec->getVersion(), dbcppp_NetworkGetVersion(impl));
-	
-	BOOST_TEST_MESSAGE("Done!");
+    const dbcppp_Network* impl = dbcppp_NetworkLoadDBCFromFile(TEST_DBC_WITH_SINGLE_COMMENTS);
+    BOOST_REQUIRE_MESSAGE(impl, "C API DBC parsing failed!");
+
+    auto spec = dbcppp::Network::fromDBC(dbc_file);
+    BOOST_REQUIRE_MESSAGE(spec, "C++ API DBC parsing failed!");
+
+    BOOST_REQUIRE_EQUAL(spec->getVersion(), dbcppp_NetworkGetVersion(impl));
+
+    BOOST_TEST_MESSAGE("Done!");
 }
