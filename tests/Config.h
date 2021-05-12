3018948e6e1007179c036d212d9c1e53ad9643f5
xR3b0rn <6249046+xR3b0rn@users.noreply.github.com>
2021-05-12 17:59:42 +0200

    Enh/tests (#54)
    * Enhanced tests
    
    * Update CMakeLists.txt

--- tests/Config.h
+++
@@ -1,3 +0,0 @@
-#define TEST_DBC "C:/hij/github/dbcppp-feat-unify-containers/dbcppp/tests/Test.dbc"
-#define TEST_DBC_WITH_SINGLE_COMMENTS "C:/hij/github/dbcppp-feat-unify-containers/dbcppp/tests/Test_single_comments.dbc"
-
